6b8f7076d47271bb23cdfb3400c38ceb2bc862e3
Craig Bakke <casbakke@gmail.com>
2025-09-18 09:35:17 -0400

    initial commit for iOS

---
+++ iOS/LostAndFound/LostAndFound/LostAndFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostAndFoundApp.swift
+//  LostAndFound
+//
+//  Created by Craig Bakke on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct LostAndFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
