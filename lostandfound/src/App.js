89ce6e2558ffa87369cf9dcfca69d96d5ddf4748
Sammy Liao <sammy.liaop@gmail.com>
2025-11-20 09:10:07 -0500

    tailwind installed

--- lostandfound/src/App.js
+++ lostandfound/src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="w-[1440px] h-[2115px] relative bg-white overflow-hidden">
+        <img class="w-[1280px] h-[640px] left-[80px] top-[-960px] absolute rounded-lg" src="https://placehold.co/1280x640" />
+        <img class="w-[1280px] h-[640px] left-[80px] top-[374px] absolute rounded-lg" src="https://placehold.co/1280x640" />
+    <div class="w-[1440px] h-52 left-0 top-0 absolute bg-amber-500 inline-flex flex-col justify-start items-start gap-10">
+        <div class="self-stretch flex flex-col justify-start items-start gap-6">
+            <div class="self-stretch text-center justify-center text-black text-6xl font-bold font-['IBM_Plex_Sans']"><br/>WIT Lost and Found</div>
+        </div>
+    </div>
+    <div class="w-[733px] left-[80px] top-[1065px] absolute inline-flex flex-col justify-center items-start gap-6">
+        <div class="w-[1188px] justify-center text-black/75 text-2xl font-normal font-['Inter'] leading-9">*Items will only be held for up to 30 days.</div>
+    </div>
+    <div class="w-44 h-20 px-6 py-3.5 left-[636px] top-[1192px] absolute bg-black rounded-lg shadow-[0px_1px_2px_0px_rgba(0,0,0,0.05)] inline-flex justify-center items-center gap-2">
+        <div class="justify-center text-white text-base font-medium font-['Inter'] leading-6">Log In</div>
+    </div>
+</div>
     </div>
   );
 }
